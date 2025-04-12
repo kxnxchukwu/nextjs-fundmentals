0d2a1b778a5f0c01aafbb2f2c179dfdeda17aaa8
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2025-04-12 22:49:14 +0100

    Add New Issues Page

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,10 @@
+export default function NewIssuePage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">New Issue Page</h1>
+      <p className="mt-4 text-lg">
+        This is the new issue page of the application.
+      </p>
+    </div>
+  )
+}
