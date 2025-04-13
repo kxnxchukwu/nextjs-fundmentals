2b00831d45eba3f1b845d173da57f505b632f6e9
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2025-04-13 20:31:54 +0100

    Add Issue Loading

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">Loading...</h1>
+      <p className="mt-4 text-lg">Please wait while we load the page.</p>
+    </div>
+  )
+}
