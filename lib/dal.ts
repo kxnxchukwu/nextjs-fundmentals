a61da5dcc0a83e3c102da25dc5b2386f53a4e59d
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2025-04-13 20:34:52 +0100

    Add getCurrentUser, getUserByEmail, getIssues, getIssue utils. Creating cacheTag on getIssues.

--- lib/dal.ts
+++ lib/dal.ts
@@ -2,5 +2,70 @@ import { db } from '@/db'
 import { getSession } from './auth'
 import { eq } from 'drizzle-orm'
 import { cache } from 'react'
+import { unstable_cacheTag as cacheTag } from 'next/cache'
 import { issues, users } from '@/db/schema'
 import { mockDelay } from './utils'
+
+export const getCurrentUser = async () => {
+  const session = await getSession()
+  if (!session) {
+    return null
+  }
+
+  try {
+    const result = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session.userId))
+    return result[0] ?? null
+  } catch (error) {
+    console.error('Error getting user by ID:', error)
+    return null
+  }
+}
+
+export const getUserByEmail = async (email: string) => {
+  try {
+    const user = await db.query.users.findFirst({
+      where: eq(users.email, email),
+    })
+    return user ?? null
+  } catch (error) {
+    console.error('Error getting user by email:', error)
+    return null
+  }
+}
+
+export async function getIssues() {
+  'use cache'
+  cacheTag('issues')
+  try {
+    await mockDelay(800)
+    const result = await db.query.issues.findMany({
+      with: {
+        user: true,
+      },
+      orderBy: (issues, { desc }) => [desc(issues.createdAt)],
+    })
+    return result
+  } catch (error) {
+    console.error('Error fetching issues:', error)
+    throw new Error('Failed to fetch issues')
+  }
+}
+
+export async function getIssue(id: number) {
+  try {
+    await mockDelay(800)
+    const issue = db.query.issues.findFirst({
+      where: eq(issues.id, id),
+      with: {
+        user: true,
+      },
+    })
+    return issue
+  } catch (error) {
+    console.error('Error fetching issue:', error)
+    return null
+  }
+}
